e83da9a4d01ed943bb02f38eeb3a2a13de73507e
jose lima <jlima@lan111.homenet.local>
2021-03-14 13:55:51 -0500

    maps semi done

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,7 +1,14 @@
+-- todo: reload init.lua??
+-- nnoremap <silent><leader>t :silent !tmux send-keys -t 2 c-p Enter<cr>
 local map = vim.api.nvim_set_keymap
 
 options = { noremap = true }
 
+-- leader
+map('n', '<Space>', '', {})
+vim.g.mapleader = ' '  -- 'vim.g' sets global variables
+
+-- pure sauce
 map('i', 'jk', '<esc><cmd>noh<cr><c-g>', options)
 
 -- disable c-z (bg)
@@ -11,55 +18,30 @@ map('c', '<c-z>', '<nop>', options)
 map('n', 'cp', 'yap<S-}>p', options)
 
 -- macros
-
 map('n', 'Q', '@q', options)
-map('x', 'Q', ":'<,'>norm @q<cr>", options)
 
-test
-test
-test
+map('n', '<leader>ve', ':e $MYVIMRC<cr>', options)
 
 
--- " macros
--- nnoremap Q @q
--- xnoremap Q :'<,'>norm @q<cr>
--- 
--- " create list from visual select and normal mode -> single line
--- " xnoremap <C-l> :s/\v\s+$//ge<cr>gv :s/\v^(.+)$/"\1",/ge<cr>gvJ :noh<cr>
--- 
--- nnoremap <leader>ve :e $MYVIMRC<cr>
--- nnoremap <leader>vr :source $MYVIMRC<cr>
--- 
--- inoremap jk <Esc>:noh<cr><c-g>
--- 
--- " move around line wraps
--- nnoremap <expr> k v:count == 0 ? 'gk' : 'k'
--- nnoremap <expr> j v:count == 0 ? 'gj' : 'j'
--- 
--- nnoremap <silent><leader>t :silent !tmux send-keys -t 2 c-p Enter<cr>
--- 
--- nnoremap H ^
--- nnoremap L g_
--- vnoremap H ^
--- vnoremap L g_
--- 
--- nnoremap U <C-r>
--- 
--- cnoremap w!! w !sudo tee % > /dev/null
--- cnoremap %s %s/\v
--- cnoremap %g %g/\v
--- cnoremap %v %v/\v
--- 
--- " improved jumps w/marks (marks go to col ' `)
--- nnoremap ' `
--- nnoremap / ms/\v
--- xnoremap / ms/\v
--- nnoremap ? ms?\v
--- xnoremap ? ms?\v
--- nnoremap gg msgg<c-g>
--- nnoremap G msG<c-g>
--- nnoremap gd msgd
--- nnoremap * ms*
--- nnoremap # ms#
--- 
--- nmap V Vg_
+map('n', 'H', '^', options)
+map('n', 'L', 'g_', options)
+map('v', 'H', '^', options)
+map('v', 'L', 'g_', options)
+map('n', 'U', '<c-r>', options)
+map('c', '%s', [[%s/\v]], options)
+map('c', '%g', [[%g/\v]], options)
+map('c', '%v', [[%v/\v]], options)
+
+-- marks/jumps
+map('n', '\'', '`', options)
+map('n', '/', [[ms/\v]], options)
+map('x', '/', [[ms/\v]], options)
+map('n', '?', [[ms?\v]], options)
+map('x', '?', [[ms?\v]], options)
+map('n', 'gg', 'msgg<c-g>', options)
+map('n', 'G', 'msG<c-g>', options)
+map('n', 'gd', 'msgd', options)
+map('n', '*', 'ms*', options)
+map('n', '#', 'ms#', options)
+map('n', 'V', 'Vg_', {})
+
