59c50216af59084a078537ec6cf643eb8d1d8cb3
lima <lima@mbp>
2021-03-15 20:29:02 -0500

    updates

--- nvim/lua/vars.lua
+++ nvim/lua/vars.lua
@@ -174,6 +174,7 @@ gs("vim_tree_ignore", {".git", "node_modules", ".cache"})
 g.nvim_tree_quit_on_open = 1
 g.nvim_tree_follow = 1
 g.nvim_tree_width_allow_resize = 1
+g.nvim_tree_git_hl = 1
 -- gs("nvim_tree_show_icons", {git = 1, folders = 0, files = 0})
 
 --[[
