e111159d03e2323af090b32bbd78f1825441c697
lima <lima@mbp>
2021-03-17 20:40:09 -0500

    updates

--- nvim/init.lua
+++ nvim/init.lua
@@ -39,9 +39,6 @@ m("c", "<c-z>", "<nop>", options)
 m("n", "cp", "yap<S-}>p", options)
 
 function _G.better_insert()
-  -- _G global lua obj exposed via v:lua in vim
-  -- "_ddO -> "_ preserves the register
-  -- local col = vim.fn.col(".") - 1
   local line = vim.api.nvim_get_current_line()
   if #line == 0 then
     return '"_ddO'
