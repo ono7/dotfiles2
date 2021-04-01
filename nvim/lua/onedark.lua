0f5eed3565c9578ef27f4d55a9592704dc6d7e25
lima <lima@mbp>
2021-04-01 08:50:05 -0500

    updates

--- nvim/lua/onedark.lua
+++ nvim/lua/onedark.lua
@@ -220,7 +220,7 @@ M.setup = function()
 
   -- -- Variable
   -- M.highlight("TSVariable", {})
-  M.highlight("TSVariableBuiltin", {fg = M.colors.yellow, gui = "bold"})
+  M.highlight("TSVariableBuiltin", {fg = M.colors.yellow, gui = "italic"})
 
   -- -- Text
   -- M.highlight("TSText", {})
