3951d7e973d9ffb72cc99c266640eb69ae94b667
lima <lima@mbp>
2021-04-11 08:29:55 -0500

    updates

--- nvim/lua/my_maps.lua
+++ nvim/lua/my_maps.lua
@@ -69,6 +69,8 @@ m("n", "<c-p>", "<Plug>(ale_previous_wrap)", {silent = true})
 -- fzf
 m("n", "<leader>f", ":Files<cr>", silent)
 m("n", "<leader>b", ":Buffers<cr>", silent)
+m("n", "<leader>s", ":Rg<cr>", silent)
+-- m("n", "<leader>c", ":Commits<cr>", silent) -- requires fugitive
 
 vim.g.surround_no_mappings = 1
 m("n", "S", "<Plug>YSurround", {})
