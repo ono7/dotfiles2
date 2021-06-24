1df6220b7b169845dfdeb08471b0b984b5767daf
lima <lima@mbp>
2021-06-23 20:55:43 -0500

    updates

--- pdbrc.py
+++ pdbrc.py
@@ -18,16 +18,10 @@ class Config(pdb.DefaultConfig):
     encoding = "utf-8"
     sticky_by_default = True
     truncate_long_lines = False
-
-    #     prompt = "pdb> "
     current_line_color = 154
-    #     editor = "${EDITOR:-vim}"
-
-    #     def __init__(self):
-    #         pass
-
-    def setup(self, pdb):
-        # make 'l' an alias to 'longlist'
-        # Pdb.do_l = Pdb.do_longlist
-        Pdb = pdb.__class__
-        Pdb.do_st = Pdb.do_sticky
+    bg = "dark"
+    # def setup(self, pdb):
+    #     # make 'l' an alias to 'longlist'
+    #     # Pdb.do_l = Pdb.do_longlist
+    #     Pdb = pdb.__class__
+    #     Pdb.do_st = Pdb.do_sticky
