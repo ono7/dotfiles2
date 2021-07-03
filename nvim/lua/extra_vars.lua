cab172b3201f51df3b43af8ac62058f0b9ff8281
lima <lima@mbp>
2021-07-03 11:06:34 -0500

    updates

--- nvim/lua/extra_vars.lua
+++ nvim/lua/extra_vars.lua
@@ -11,10 +11,11 @@ let g:pyindent_open_paren = '0'
 let g:pyindent_nested_paren = '&sw'
 let g:pyindent_continue = '&sw'
 
-let g:netrw_banner=0
-let g:netrw_browse_split=4
-let g:netrw_altv=1
-let g:netrw_liststyle=3
+let g:netrw_banner = 0
+let g:netrw_liststyle = 3
+let g:netrw_browse_split = 4
+let g:netrw_altv = 1
+let g:netrw_winsize = 20
 let g:netrw_list_hide=netrw_gitignore#Hide()
 
 set synmaxcol=512
