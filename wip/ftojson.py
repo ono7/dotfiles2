d01ebb4ca7ca2e2a61808cc6c9755794db58b1e4
lima <lima@mbp>
2021-07-22 03:37:00 -0500

    updates

--- wip/ftojson.py
+++ wip/ftojson.py
@@ -11,6 +11,8 @@ import regex as re
 from json import dumps
 
 data = """auth radius-server /Common/system_auth_name1 {
+    single_line2 { test2 test2 test2 test3 }
+    single_line3 { }
     secret superdupersecret=
     server 10.1.1.1
     test {
@@ -21,7 +23,7 @@ data = """auth radius-server /Common/system_auth_name1 {
 """
 
 
-def getKeys(line):
+def get_keys(line):
     """returns keys to be used for dict nodes
     level1 is root key, level2 is to be used for nesting
     { "level1" : { "level2": {}}}
@@ -36,13 +38,25 @@ def getKeys(line):
     return level1, level2
 
 
+def get_single_line_item(line):
+    """if we find a single line with {}
+    we will extract key and values as a list
+    """
+    g1, g2 = re.search("(\S+).*?{([^{}]*)}", line).groups()
+    return g1, g2.split()
+
+
 def ret_obj(data):
+    """recursively do magical things"""
     node = {}
     for index, line in enumerate(data):
         if line.strip() == "}":
             return node
-        if "{" in line:
-            level1, level2 = getKeys(line)
+        if len(re.findall("[{}]", line)) == 2:
+            level1, results = get_single_line_item(line)
+            node.setdefault(level1, results)
+        elif "{" in line:
+            level1, level2 = get_keys(line)
             if level2:
                 node.setdefault(level1, {}).setdefault(level2, {})
                 results = ret_obj(data[index + 1 :])
