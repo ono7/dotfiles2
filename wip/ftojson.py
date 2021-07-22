0a65b3e4f8ee44c73a8fabd4c77cd422aea0b337
lima <lima@mbp>
2021-07-22 04:14:15 -0500

    updates

--- wip/ftojson.py
+++ wip/ftojson.py
@@ -10,15 +10,32 @@
 import regex as re
 from json import dumps
 
-data = """auth radius-server /Common/system_auth_name1 {
-    single_line2 { test2 test2 test2 test3 }
-    single_line3 { }
-    secret superdupersecret=
-    server 10.1.1.1
-    test {
-         test1 inner1
-         test2 inner1
-   }
+# data = """auth radius-server /Common/system_auth_name1 {
+#     single_line2 { test2 test2 test2 test3 }
+#     single_line3 { }
+#     secret superdupersecret=
+#     server 10.1.1.1
+#     test {
+#          test1 inner1
+#          test2 inner1
+#    }
+# }
+# """
+data = """ltm virtual export_me {
+    destination 10.1.30.30:https
+    ip-protocol tcp
+    mask 255.255.255.255
+    policies {
+        linux-high { }
+    }
+    pool test-pool
+    source 0.0.0.0/0
+    source-address-translation {
+        type automap
+    }
+    translate-address enabled
+    translate-port enabled
+    vs-index 2
 }
 """
 
@@ -46,25 +63,33 @@ def get_single_line_item(line):
     return g1, g2.split()
 
 
-def ret_obj(data):
+def ret_obj(data, s=None):
     """recursively do magical things"""
     node = {}
+    stack = []
+    if s:
+        stack = s
     for index, line in enumerate(data):
         if line.strip() == "}":
-            return node
+            stack.pop()
+            if len(stack) == 0:
+                return node
+            else:
+                continue
         if len(re.findall("[{}]", line)) == 2:
             level1, results = get_single_line_item(line)
             node.setdefault(level1, results)
         elif "{" in line:
+            stack.append("{")
             level1, level2 = get_keys(line)
             if level2:
                 node.setdefault(level1, {}).setdefault(level2, {})
-                results = ret_obj(data[index + 1 :])
+                results = ret_obj(data[index + 1 :], stack)
                 node[level1][level2].update(results)
                 return node
             else:
                 node.setdefault(level1, {})
-                results = ret_obj(data[index + 1 :])
+                results = ret_obj(data[index + 1 :], stack)
                 node[level1].update(results)
                 return node
 
