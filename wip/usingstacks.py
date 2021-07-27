896d735e5e751b24bbb2e8cc5989a416c2d3398d
lima <lima@mbp>
2021-07-27 02:29:43 -0500

    updates

--- wip/usingstacks.py
+++ wip/usingstacks.py
@@ -15,14 +15,6 @@ from util import (
     is_parent,
 )
 
-lines = """ltm virtual export_me {
-    policies {
-        linux-high { }
-    }
-    pool test-pool
-    pool1 test-pool
-}
-"""
 
 # lines = """ltm virtual export_me {
 #     pool test-pool
@@ -59,6 +51,15 @@ lines = """ltm virtual export_me {
 # }
 # """
 
+lines = """ltm virtual export_me {
+    policies {
+        linux-high { }
+    }
+    pool test-pool
+    pool1 test-pool
+}
+"""
+
 data = clean_data_chunk(lines)
 
 
@@ -73,6 +74,8 @@ def create_new_objects(line, node=None, level=None):
     """
     new_node = Storage(*is_parent(line))
     if node:
+        if node.parent:
+            node.parent.update(node.get_store())
         storage_stack.insert(level, new_node)
         new_node.parent = node
     else:
@@ -85,21 +88,22 @@ def create_new_objects(line, node=None, level=None):
 
 level = -1
 for line in lines.splitlines():
-    __import__("ipdb").set_trace(context=5)
     if line.strip() == "}" and stack.is_balanced():
         level -= 1
+        if node.parent:
+            node.parent.update(node.get_store())
         continue
     if line.strip() == "}":
         stack.update_state(line)
         if stack.is_balanced() and len(stack_of_stacks) != 0:
             stack = stack_of_stacks.pop()
             level -= 1
+            if node.parent:
+                node.parent.update(node.get_store())
             continue
     if line.endswith("{"):
         level += 1
         try:
-            if node.parent:
-                node.parent.update(node.get_store())
             stack, node = create_new_objects(line, node, level)
         except NameError:
             stack, node = create_new_objects(line)
@@ -112,21 +116,4 @@ print(stack_of_stacks)
 print(storage_stack)
 
 root = storage_stack.pop(0)
-# while len(storage_stack) != 0:
-#     n = storage_stack.pop(0)
-#     root.update(n.get_store())
-
 print(dumps(root.get_store(), indent=2))
-
-todos = """
-done 1. strip first line to create root object
-done 2. parse until we find another parent, push old parent to stack, update current object
-done -> using obj stack 4. keep track of objects and update sequences
-    factory function?
-done 5. find end of current object, using Stack() and update final structure and pop from stack
-done 6. update parent object
-done 7. if line is not parent object, continue updating current opbject
-
-8. if nested object, should update last object
-
-"""
