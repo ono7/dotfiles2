f5aff2bce28302330626de956e7251c462b06606
lima <lima@mbp>
2021-07-29 08:15:17 -0500

    updates

--- nvim/lua/extra_vars.lua
+++ nvim/lua/extra_vars.lua
@@ -258,9 +258,13 @@ hi!  ModeMsg       term=italic   ctermfg=8      gui=italic     guifg=#939eb3
 hi!  MoreMsg       term=italic   ctermfg=7      gui=italic     guifg=#939eb3
 hi!  MsgArea       ctermfg=8     guifg=#939eb3
 hi!  DiffAdd       ctermfg=0     ctermbg=2      gui=none        guifg=Black           guibg=#98C379
-hi!  DiffDelete    ctermfg=0     ctermbg=1      gui=none        guifg=Black           guibg=#E06C75
-hi!  DiffChange    ctermfg=0     ctermbg=4      gui=none        guifg=Black           guibg=#61AFEF
+hi!  DiffDelete    ctermfg=1     ctermbg=0      gui=none        guifg=Black           guibg=#E06C75
+hi!  DiffChange    ctermfg=none  ctermbg=none   gui=none        guifg=Black           guibg=#61AFEF
 hi!  DiffText      ctermfg=0     ctermbg=3      gui=none        guifg=Black           guibg=#e5c07b
+" hi!  DiffAdd       ctermfg=0     ctermbg=2      gui=none        guifg=Black           guibg=#98C379
+" hi!  DiffDelete    ctermfg=0     ctermbg=1      gui=none        guifg=Black           guibg=#E06C75
+" hi!  DiffChange    ctermfg=0     ctermbg=4      gui=none        guifg=Black           guibg=#61AFEF
+" hi!  DiffText      ctermfg=0     ctermbg=3      gui=none        guifg=Black           guibg=#e5c07b
 hi!  Folded        ctermfg=8     guifg=#5c6370  ctermbg=none    guibg=none            gui=bold            term=bold
 hi!  FoldColumn    ctermbg=none  guibg=#3E4452
 hi!  SignColumn    ctermfg=7     ctermbg=none
