12389f72a78222cb6f5dc53073ad8ca1acdfc078
lima <lima@mbp>
2021-08-18 11:16:07 -0500

    updates

--- nvim/init.lua
+++ nvim/init.lua
@@ -152,7 +152,6 @@ function _G.perflog()
 end
 
 require "onedark".setup {}
--- cmd [[colorscheme base16-onedark]]
 cmd "hi!  snipLeadingSpaces  ctermbg=0  guibg=none  guifg=none"
 
 require "my_maps"
