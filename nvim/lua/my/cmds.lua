3b1e840c153030aa2718085218be4015368f93f5
lima <lima@mbp>
2023-04-05 11:48:55 -0500

    updates

--- nvim/lua/my/cmds.lua
+++ nvim/lua/my/cmds.lua
@@ -19,12 +19,6 @@ c("TextYankPost", {
 c({ "VimResized" }, { pattern = "*", command = [[:wincmd =]], group = mygrp })
 
 -- restore cursor position on enter
--- c({ "BufReadPost" }, { -- old way
--- 	pattern = "*",
--- 	command = [[if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g`\"" | endif]],
--- 	group = mygrp,
--- })
--- restore cursor position on enter
 vim.api.nvim_create_autocmd("BufRead", {
 	callback = function(opts)
 		vim.api.nvim_create_autocmd("BufWinEnter", {
@@ -43,9 +37,20 @@ vim.api.nvim_create_autocmd("BufRead", {
 			end,
 		})
 	end,
+	group = mygrp,
 })
+
 -- set my fo options
-c({ "BufEnter" }, { pattern = "*", command = [[setlocal formatoptions=qnlj]], group = mygrp })
+-- 2023-04-05 disabled... testing
+-- c({ "BufEnter" }, { pattern = "*", command = [[setlocal formatoptions=qnlj]], group = mygrp })
+
+c("BufEnter", {
+	callback = function()
+		vim.opt.formatoptions:remove({ "c", "r", "o" })
+	end,
+	group = mygrp,
+	desc = "Disable New Line Comment",
+})
 
 -- c({"Syntax"}, {pattern = "*", command = "syntax sync minlines=200", group = mygrp })
 
@@ -63,7 +68,11 @@ c({
 })
 
 -- auto source snippets file
-c({ "BufWritePost" }, { pattern = "snippet.lua", command = [[:luafile %]], group = mygrp })
+c("BufWritePost", {
+	pattern = "snippet.lua",
+	command = [[:luafile %]],
+	group = mygrp,
+})
 
 -- auto create dirs when saving files
 c("BufWritePre", {
@@ -78,4 +87,57 @@ c({ "BufWritePre" }, {
 	callback = function()
 		vim.lsp.buf.format()
 	end,
+})
+
+c("FocusGained", {
+	callback = function()
+		vim.cmd("checktime")
+	end,
+	group = mygrp,
+	desc = "Update file when there are changes",
+})
+
+-- autocmd("ModeChanged", {
+--   callback = function()
+--     if fn.getcmdtype() == "/" or fn.getcmdtype() == "?" then
+--       vim.opt.hlsearch = true
+--     else
+--       vim.opt.hlsearch = false
+--     end
+--   end,
+--   group = mygrp,
+--   desc = "Highlighting matched words when searching",
+-- })
+
+-- remove this if there's an issue
+-- c({ "BufReadPost", "BufNewFile" }, {
+--   once = true,
+--   callback = function()
+--     if vim.fn.has "wsl" == 1 then
+--       -- Set a compatible clipboard manager
+--       vim.g.clipboard = {
+--         copy = {
+--           ["+"] = "win32yank.exe -i --crlf",
+--           ["*"] = "win32yank.exe -i --crlf",
+--         },
+--         paste = {
+--           ["+"] = "win32yank.exe -o --lf",
+--           ["*"] = "win32yank.exe -o --lf",
+--         },
+--       }
+--     end
+--     vim.opt.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
+--   end,
+--   group = augroup,
+--   desc = "Lazy load clipboard",
+-- })
+
+c("TermOpen", {
+	callback = function()
+		vim.opt_local.relativenumber = false
+		vim.opt_local.number = false
+		vim.cmd("startinsert!")
+	end,
+	-- group = augroup,
+	desc = "Terminal Options",
 })
