6fe33831119c577432a7c8696db72c5b828a1104
lima <lima@mbp>
2023-04-10 20:17:26 -0500

    updates

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,85 +1,85 @@
 local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 
 if not status_ok then
-	print("problem loading treesitter - plugins/treesitter.lua")
-	return
+  print("problem loading treesitter - plugins/treesitter.lua")
+  return
 end
 
 configs.setup({
-	auto_install = false,
-	indent = { enable = true, disable = { "python" } },
-	highlight = {
-		max_file_lines = 10000,
-		enable = true,
-		additional_vim_regex_highlighting = false,
-		indent = { enable = false },
-		incremental_selection = { enable = false },
-		textobjects = { enable = true },
-		rainbow = {
-			max_file_lines = 5000,
-		},
-		disable = function(lang, bufnr)
-			return vim.fn.getfsize(vim.api.nvim_buf_get_name(bufnr)) > 1048576
-		end,
-	},
-	ensure_installed = {
-		-- "help", -- vim help
-		"bash",
-		"go",
-		"javascript",
-		"typescript",
+  auto_install = false,
+  indent = { enable = true, disable = { "python" } },
+  highlight = {
+    max_file_lines = 10000,
+    enable = true,
+    additional_vim_regex_highlighting = false,
+    indent = { enable = false },
+    incremental_selection = { enable = false },
+    textobjects = { enable = true },
+    rainbow = {
+      max_file_lines = 5000,
+    },
+    disable = function(lang, bufnr)
+      return vim.fn.getfsize(vim.api.nvim_buf_get_name(bufnr)) > 1048576
+    end,
+  },
+  ensure_installed = {
+    "vimdoc",
+    "bash",
+    "go",
+    "javascript",
+    "typescript",
     "markdown",
     "terraform",
-		"json",
-		"lua",
-		"make",
-		"python",
-		"regex",
-		"vim",
-		"yaml",
-	},
-	textobjects = {
-		select = {
-			enable = true,
-			lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-			keymaps = {
-				-- You can use the capture groups defined in textobjects.scm
-				["aa"] = "@parameter.outer",
-				["ia"] = "@parameter.inner",
-				["af"] = "@function.outer",
-				["if"] = "@function.inner",
-				["ac"] = "@class.outer",
-				["ic"] = "@class.inner",
-			},
-		},
-		move = {
-			enable = true,
-			set_jumps = true, -- whether to set jumps in the jumplist
-			goto_next_start = {
-				["]m"] = "@function.outer",
-				["]]"] = "@class.outer",
-			},
-			goto_next_end = {
-				["]M"] = "@function.outer",
-				["]["] = "@class.outer",
-			},
-			goto_previous_start = {
-				["[m"] = "@function.outer",
-				["[["] = "@class.outer",
-			},
-			goto_previous_end = {
-				["[M"] = "@function.outer",
-				["[]"] = "@class.outer",
-			},
-		},
-		swap = {
-			enable = true,
-			swap_next = {
-				["<leader>a"] = "@parameter.inner",
-			},
-			swap_previous = {
-				["<leader>A"] = "@parameter.inner",
-			},
-		},
-	},
+    "json",
+    "lua",
+    "make",
+    "python",
+    "regex",
+    "vim",
+    "yaml",
+  },
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ["aa"] = "@parameter.outer",
+        ["ia"] = "@parameter.inner",
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
+      },
+    },
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        ["]m"] = "@function.outer",
+        ["]]"] = "@class.outer",
+      },
+      goto_next_end = {
+        ["]M"] = "@function.outer",
+        ["]["] = "@class.outer",
+      },
+      goto_previous_start = {
+        ["[m"] = "@function.outer",
+        ["[["] = "@class.outer",
+      },
+      goto_previous_end = {
+        ["[M"] = "@function.outer",
+        ["[]"] = "@class.outer",
+      },
+    },
+    swap = {
+      enable = true,
+      swap_next = {
+        ["<leader>a"] = "@parameter.inner",
+      },
+      swap_previous = {
+        ["<leader>A"] = "@parameter.inner",
+      },
+    },
+  },
 })
