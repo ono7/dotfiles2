cce07575ce31acacc01ee2e572a6227a49db8f92
lima <lima@mbp>
2023-05-05 07:05:53 -0500

    updates

--- golang/http_calls/main.go
+++ golang/http_calls/main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
+	"strings"
 )
 
 // body: {"page":"words","input":"","words":["word1","word2","word3"]}
 
-type words struct {
-	Page  string   `json:"page"`
+type Words struct {
+	Page  string   `json:"page"` // extract fields from json for use in struct values
 	Input string   `json:"input"`
 	Words []string `json:"words"`
 }
@@ -38,6 +40,16 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Status: %d\n\n", response.StatusCode)
-	fmt.Printf("body: %s\n", string(r))
+	if response.StatusCode != 200 {
+		log.Fatal("Wrong status code: ", response.StatusCode)
+	}
+
+	var words Words
+	err = json.Unmarshal(r, &words) // view signature to figure out what this returns and that it needs a pointer
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Page: %s, Words -> %s", words.Page, strings.Join(words.Words, ", "))
+
 }
