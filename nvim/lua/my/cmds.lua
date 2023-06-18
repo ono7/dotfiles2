4788cf11be972b22e330e66012af43c74bc0a14f
lima <lima@mbp>
2023-06-18 08:50:00 -0500

    updates

--- nvim/lua/my/cmds.lua
+++ nvim/lua/my/cmds.lua
@@ -45,6 +45,27 @@ vim.api.nvim_create_autocmd("BufRead", {
 
 c({ "BufEnter" }, { pattern = "*.go", command = [[:LspStart]], group = mygrp })
 
+-- c("BufWritePre", {
+--   pattern = { "*.go" },
+--   callback = function()
+--
+--     local save_cursor = vim.fn.getcurpos()
+--     local params = vim.lsp.util.make_range_params(nil, "utf-16")
+--     params.context = { only = { "source.organizeImports" } }
+--     local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+--     for _, res in pairs(result or {}) do
+--       for _, r in pairs(res.result or {}) do
+--         if r.edit then
+--           vim.lsp.util.apply_workspace_edit(r.edit, "utf-16")
+--         else
+--           vim.lsp.buf.execute_command(r.command)
+--         end
+--       end
+--     end
+--     vim.fn.setpos('.', save_cursor)
+--   end,
+-- })
+
 c("BufEnter", {
   callback = function()
     vim.opt.formatoptions:remove({ "c", "r", "o" })
