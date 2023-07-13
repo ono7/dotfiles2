76130566b8f696c12689db772cc3b707cd37f615
lima <lima@mbp>
2023-07-13 10:36:37 -0500

    updates

--- nvim/init.lua
+++ nvim/init.lua
@@ -125,10 +125,10 @@ vim.api.nvim_set_keymap('n', '<leader>y', [[:lua send_to_tmux(vim.fn.getline('.'
 k("n", "<leader>cd", ":lcd %:h<CR>")
 
 -- paste over selection without overwriting clipboard
-k("x", "p", "pgvy")
-k("v", "p", [["0p]])
-k("v", "P", [["0P]])
-k("v", "d", [["0d]])
+k("x", "p", "pgvy") -- prevent paste from overwritting what was copied originally
+-- k("v", "p", [["0p]])
+-- k("v", "P", [["0P]])
+-- k("v", "d", [["0d]])
 
 k({ "n", "v" }, "J", "mzJ`z") -- when using J keep cursor to the right
 
