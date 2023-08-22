d707dfb0acce7b18996434f6614e0354f21c4eb0
lima <lima@mbp>
2023-08-22 00:32:38 -0500

    updates -> nvim/lua/my/lazy.lua nvim/lua/plugins/floaterm.lua nvim/lua/plugins/theme_catppuccin.lua nvim/snippets/make.snippets

--- nvim/lua/my/lazy.lua
+++ nvim/lua/my/lazy.lua
@@ -44,7 +44,20 @@ require("lazy").setup({
       { "williamboman/mason-lspconfig.nvim" },
     },
   },
-  { "numtostr/fterm.nvim" },
+  {
+    "numtostr/fterm.nvim",
+    opts = {},
+    config = function()
+      require('FTerm').setup {
+        dimensions = {
+          height = 0.9,
+          width = 0.9,
+        }
+      }
+      vim.keymap.set("n", "<c-t>", '<CMD>lua require("FTerm").toggle()<CR>', { noremap = true, silent = true })
+      vim.keymap.set("t", "<c-t>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', { noremap = true, silent = true })
+    end
+  },
   {
     -- npm install -g yarn
     -- or
--- nvim/lua/plugins/floaterm.lua
+++ nvim/lua/plugins/floaterm.lua
@@ -1,10 +1,17 @@
-local status_ok, Fterm = pcall(require, "Fterm")
+-- local status_ok, fterm_config = pcall(require, "Fterm")
+
 
 -- if not status_ok then
 --   print("Fterm not loaded - plugins/Fterm.lua")
 --   return
 -- end
+-- fterm_config.setup({
+--   dimensions = {
+--     height = 0.9,
+--     width = 0.9
+--   }
+-- })
 
-vim.keymap.set("n", "<c-t>", '<CMD>lua require("FTerm").toggle()<CR>', { noremap = true, silent = true })
--- vim.keymap.set("n", "<c-t>", '<CMD>lua require("FTerm").run({ "cd", "%:p:h" })<CR>', { noremap = true, silent = true })
-vim.keymap.set("t", "<c-t>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', { noremap = true, silent = true })
+
+-- vim.keymap.set("n", "<c-t>", '<CMD>lua require("FTerm").toggle()<CR>', { noremap = true, silent = true })
+-- vim.keymap.set("t", "<c-t>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', { noremap = true, silent = true })
