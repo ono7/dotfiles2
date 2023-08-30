d85f510f5ac651d35193dcd075ce993c31d7b09e
Jose L <20385388+ono7@users.noreply.github.com>
2023-08-30 09:19:02 -0500

    Update wezterm.windows.lua

--- wezterm/wezterm.windows.lua
+++ wezterm/wezterm.windows.lua
@@ -1,4 +1,32 @@
 local wezterm = require("wezterm")
+local scheme = wezterm.get_builtin_color_schemes()['Catppuccin Mocha']
+-- from wezterm debug mode see keybinding below
+-- [
+--     "#2d2e3e",
+--     "#e5e693",
+--     "#685da5",
+--     "#ac68a6",
+--     "#a9cee1",
+--     "#667684",
+--     "#7c527f",
+--     "#4c5568",
+--     "#8174d3",
+--     "#b3da99",
+-- ]
+
+local ansi = {
+  "#45475a",
+  "#f38ba8",
+  "#ceeac8",
+  "#f9e2af",
+  "#89b4fa",
+  "#cba6f7",
+  "#94e2d5",
+  "#bac2de",
+}
+
+scheme.ansi = ansi
+scheme.brights = ansi
 
 return {
   -- default_prog = { "cmd.exe" },
@@ -95,22 +123,27 @@ return {
   -- color_scheme = "theme_numetal", -- 1st pick
   -- front_end = "WebGpu",
   -- color_scheme = "tokyonight_storm",
-  color_scheme                               = 'mine',
+  -- color_scheme                               = 'mine',
   color_schemes                              = {
-    ['mine'] = {
-      -- background = "#222932",
-      background = "#202933",
-      -- foreground = "#dee6f0",
-      foreground = "#c7cff7",
-      cursor_fg = "#323a4c",
-      cursor_bg = "#ffffff",
-      cursor_border = "#ffcc66",
-      selection_bg = "#33415e",
-      selection_fg = "#cbccc6",
-      ansi = { "#192028", "#e27e8d", "#c5dea1", "#c49a5a", "#8EAAF4", "#c6a0f6", "#5ec4ff", "#c7c7c7" },
-      brights = { "#686868", "#e27e8d", "#b6d68a", "#ffd580", "#8EAAF4", "#b7bdf8", "#70e1e8", "#ffffff", }
-    }
+    -- new or override scheme
+    ["Catppuccin Mocha"] = scheme,
   },
+  color_scheme                               = 'Catppuccin Mocha',
+  -- color_schemes                              = {
+  --   ['mine'] = {
+  --     -- background = "#222932",
+  --     background = "#202933",
+  --     -- foreground = "#dee6f0",
+  --     foreground = "#c7cff7",
+  --     cursor_fg = "#323a4c",
+  --     cursor_bg = "#ffffff",
+  --     cursor_border = "#ffcc66",
+  --     selection_bg = "#33415e",
+  --     selection_fg = "#cbccc6",
+  --     ansi = { "#192028", "#e27e8d", "#c5dea1", "#c49a5a", "#8EAAF4", "#c6a0f6", "#5ec4ff", "#c7c7c7" },
+  --     brights = { "#686868", "#e27e8d", "#b6d68a", "#ffd580", "#8EAAF4", "#b7bdf8", "#70e1e8", "#ffffff", }
+  --   }
+  -- },
   font                                       = wezterm.font({ family = "MonoLisa Liga" }),
   -- freetype_load_target = "HorizontalLcd",
   -- front_end = "OpenGL",
@@ -139,5 +172,5 @@ return {
   --   && curl -o $tempfile https://raw.githubusercontent.com/wez/wezterm/main/termwiz/data/wezterm.terminfo \
   --   && tic -x -o ~/.terminfo $tempfile \
   --   && rm $tempfile
-  term = "wezterm",
+  term                                       = "xterm-256color",
 }
