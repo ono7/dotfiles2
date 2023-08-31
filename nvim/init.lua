5bf63b0518e28f6a14946dce78e0341cdde4e895
lima <lima@mbp>
2023-08-30 23:01:33 -0500

    updates -> nvim/init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -36,9 +36,9 @@ vim.g.t_co = 256
 vim.opt.termguicolors = true
 vim.opt.syntax = "off"
 
--- if syntax is on/enabled treesitter has issues
--- other weird things happen, like lsp not starting automatically etc
--- see https://thevaluable.dev/tree-sitter-neovim-overview/
+--- if syntax is on/enabled treesitter has issues
+--- other weird things happen, like lsp not starting automatically etc
+--- see https://thevaluable.dev/tree-sitter-neovim-overview/
 
 
 P = function(x)
@@ -106,16 +106,28 @@ vim.opt.path:append({ "**" })
 --- nop ---
 k({ "c", "x", "n" }, "<c-z>", "") -- use this for searching repos
 k("n", "<c-f>", "")               -- use this for searching files
--- k("n", "<c-b>", "")               -- this conflicts with tmux...
+--- k("n", "<c-b>", "")               -- this conflicts with tmux...
 k("n", "ZZ", "")
 k("n", "ZQ", "")
--- k("n", "M", "")
--- k("n", "L", "")
--- k("n", "H", "")
+--- k("n", "M", "")
+--- k("n", "L", "")
+--- k("n", "H", "")
 
 k({ "n", "x" }, "<c-e>", "$")
 
--- surround
+--- vs last paste
+k("n", "gp", "`[v`]", silent)
+
+--- keep cursor in same position when yanking in visual
+k("v", "y", [[ygv<Esc>]], silent)
+
+k("n", ",r", vim.lsp.buf.format, silent)
+k("n", "<leader>w", ":w<cr>", silent)
+k("n", ",w", ":w<cr>", silent)
+k("n", ",q", ":q!<cr>", silent)
+k("n", "<leader>q", ":q!<cr>", silent)
+
+--- surround
 k("n", [[s"]], [[ciw"<c-r><c-p>""]])
 k("n", [[s']], [[ciw'<c-r><c-p>"']])
 
@@ -131,16 +143,16 @@ k("n", "<c-j>", "<C-W>j")
 k("n", "<c-h>", "<C-W>h")
 k("n", "<c-l>", "<C-W>l")
 
--- k("n", "<c-s-k>", "<C-W>k")
--- k("n", "<c-s-j>", "<C-W>j")
--- k("n", "<c-s-h>", "<C-W>h")
--- k("n", "<c-s-l>", "<C-W>l")
+--- k("n", "<c-s-k>", "<C-W>k")
+--- k("n", "<c-s-j>", "<C-W>j")
+--- k("n", "<c-s-h>", "<C-W>h")
+--- k("n", "<c-s-l>", "<C-W>l")
 
 
--- make dot work in visual mode
+--- make dot work in visual mode
 m("v", ".", ":norm .<cr>", opt)
 
--- macros
+--- macros
 m("v", "Q", ":'<,'>norm @q<cr>", silent)
 m("n", "Q", "@q", opt)
 
@@ -151,7 +163,7 @@ k("n", "cp", "yap<S-}>p", opt)
 k("n", "[q", ":cprev<cr>", opt)
 k("n", "]q", ":cnext<cr>", opt)
 
--- ex/command mode bindings
+--- ex/command mode bindings
 k("c", "<c-a>", "<Home>", opt)
 k("c", "<c-h>", "<Left>", opt)
 k("c", "<c-l>", "<Right>", opt)
@@ -166,10 +178,6 @@ k("n", "g}", "/}<cr>")
 k("n", "g[", [[?\v\w+\[\zs<cr>]])
 k("n", "g]", [[/\v\w+\[\zs<cr>]])
 
-k("n", "<leader>w", ":w<cr>", silent)
-k("n", ",w", ":w<cr>", silent)
-k("n", ",q", ":q!<cr>", silent)
-k("n", "<leader>q", ":q!<cr>", silent)
 
 local openBrackets = {
     ["("] = true,
@@ -264,7 +272,7 @@ k('i', '`', function()
 end
 , { expr = true })
 
---  handles ''
+---  handles ''
 k('i', "'", function()
     if vim.bo[0].buftype == 'prompt' then
         return "'"
@@ -284,7 +292,7 @@ k('i', "'", function()
 end
 , { expr = true })
 
--- handle []
+--- handle []
 k('i', '[', function()
     local line = vim.api.nvim_get_current_line()
     local col = vim.fn.col('.')
@@ -309,7 +317,7 @@ k('i', ']', function()
 end
 , { expr = true })
 
--- handle {}
+--- handle {}
 k('i', '{', function()
     local line = vim.api.nvim_get_current_line()
     local col = vim.fn.col('.')
@@ -335,7 +343,7 @@ k('i', '}', function()
 end
 , { expr = true })
 
--- handle ()
+--- handle ()
 k('i', '(', function()
     local line = vim.api.nvim_get_current_line()
     local col = vim.fn.col('.')
@@ -400,14 +408,14 @@ k("i", "<enter>", function()
     end
 end, { expr = true })
 
--- k("i", "<enter>", function()
---   -- use this one when we are not autoclosing
---   local line = vim.fn.getline(".")
---   local prev_col, _ = vim.fn.col(".") - 1, vim.fn.col(".")
---   return pair_map_2[line:sub(prev_col, prev_col)]
---       and "<enter>" .. pair_map_2[line:sub(prev_col, prev_col)] .. "<Esc>O"
---       or "<Enter>"
--- end, { expr = true })
+--- k("i", "<enter>", function()
+---   -- use this one when we are not autoclosing
+---   local line = vim.fn.getline(".")
+---   local prev_col, _ = vim.fn.col(".") - 1, vim.fn.col(".")
+---   return pair_map_2[line:sub(prev_col, prev_col)]
+---       and "<enter>" .. pair_map_2[line:sub(prev_col, prev_col)] .. "<Esc>O"
+---       or "<Enter>"
+--- end, { expr = true })
 
 --- resize window ---
 k("n", "<M-j>", [[:resize -2<cr>]], silent)
@@ -415,8 +423,29 @@ k("n", "<M-k>", [[:resize +2<cr>]], silent)
 k("n", "<M-l>", [[:vertical resize -2<cr>]], silent)
 k("n", "<M-h>", [[:vertical resize +2<cr>]], silent)
 
+--- -- Lua function to send text to Tmux
+--- _G.send_to_tmux = function(text)
+---     vim.fn.system('tmux load-buffer -w -', text)
+---     print('n |  ')
+--- end
+
+--- -- Lua function to send text to Tmux
+--- _G.send_to_tmux_visual = function()
+---     local text = get_visual_selection()
+---     if text then
+---         vim.fn.system('tmux load-buffer -w -', text)
+---         print('v |  ')
+---     end
+--- end
+
+--- -- Map the key binding for a range of text or selected text
+--- k('v', '<leader>y', [[:lua send_to_tmux_visual()<CR>]], { noremap = true, silent = true })
+
+--- -- Map the key binding for the current line (no selection)
+--- k('n', '<leader>y', [[:lua send_to_tmux(vim.fn.getline('.'))<CR>]], { noremap = true, silent = true })
+
 --- tmux ---
--- k("n", "<leader>t", [[:silent !tmux send-keys -t 2 c-p Enter<cr>]], silent)
+--- k("n", "<leader>t", [[:silent !tmux send-keys -t 2 c-p Enter<cr>]], silent)
 
 --- visual selection search ---
 k("v", "<enter>", [[y/\V<C-r>=escape(@",'/\')<CR><CR>]], silent)
@@ -425,7 +454,7 @@ k("v", "<enter>", [[y/\V<C-r>=escape(@",'/\')<CR><CR>]], silent)
 k("n", "gt", [[:GoTagAdd json<cr>]])
 
 --- marks/jumps ---
--- k("n", "'", "`", opt)
+--- k("n", "'", "`", opt)
 k("n", "mm", "mM", opt)
 k("n", "ma", "mA", opt)
 k("n", "mB", "mB", opt)
@@ -436,14 +465,34 @@ k("n", "'b", [[`B'\"]], opt)
 k("i", "<C-c>", "<Esc>", opt)
 k("n", "Y", "y$", opt)
 
--- keep cursor in the middle when using search
+k("n", "U", "<c-r>", opt)
+
+--- keep cursor in the middle when using search
 k("n", "n", "nzzzv", opt)
 k("n", "N", "Nzzzv", opt)
 k("n", "<C-d>", "<C-d>zz", opt)
 k("n", "<C-u>", "<C-u>zz", opt)
 
+--- paste over selection without overwriting clipboard
+k("x", "p", "pgvy")
+
+--- when using J keep cursor to the right
+k({ "n", "v" }, "J", "mzJ`z")
+
+
 --- terminal ---
 k("t", "<Esc>", [[<c-\><c-n>]], silent)
+
+--- leave unnnamed reg alone when changing text
+k("n", "c", '"ac')
+k("n", "C", '"aC')
+
+--- visual block by default
+k({ "n", "x" }, "v", "<c-v>")
+vim.cmd("vunmap v")
+
+
+
 
 cmd([[ packadd cfilter ]]) -- quicklist filter :cfitler[!] /expression/
 
@@ -468,26 +517,19 @@ end
 cmd([[cnoreabbrev <expr> grep  (getcmdtype() ==# ':' && getcmdline() =~# '^grep')  ? 'silent grep'  : 'grep']])
 cmd([[cnoreabbrev <expr> lgrep (getcmdtype() ==# ':' && getcmdline() =~# '^lgrep') ? 'silent lgrep' : 'lgrep']])
 
--- leave unnnamed reg alone when changing text
-k("n", "c", '"ac')
-k("n", "C", '"aC')
+--- k("n", "p", [[<Plug>(miniyank-autoput)]], {})
+--- k("n", "P", [[<Plug>(miniyank-autoPut)]], {})
 
-k({ "n", "x" }, "v", "<c-v>")
-vim.cmd("vunmap v")
-
--- k("n", "p", [[<Plug>(miniyank-autoput)]], {})
--- k("n", "P", [[<Plug>(miniyank-autoPut)]], {})
-
--- switch between current and prev file
--- k("n", "<space><space>", "<c-^>") -- code action in mason.lua
+--- switch between current and prev file
+--- k("n", "<space><space>", "<c-^>") -- code action in mason.lua
 
 --- shellcode ---
--- m(
--- 	"x",
--- 	"<space>h",
--- 	[[:s/\v\s+//ge<cr><bar> :s/\v(..)/\\\x\1/ge<cr><bar> :s/\v.*/buffer \+\= b"&"/ge<cr>:noh<cr>]],
--- 	silent
--- )
+--- m(
+--- 	"x",
+--- 	"<space>h",
+--- 	[[:s/\v\s+//ge<cr><bar> :s/\v(..)/\\\x\1/ge<cr><bar> :s/\v.*/buffer \+\= b"&"/ge<cr>:noh<cr>]],
+--- 	silent
+--- )
 
 
 local packages = {
@@ -528,7 +570,7 @@ end
 vim.cmd([[cabbrev q1 q!]])
 vim.cmd([[cabbrev qall1 qall!]])
 
--- vim.cmd([[hi! clear FloatBorder]])
+--- vim.cmd([[hi! clear FloatBorder]])
 
 vim.cmd([[
 function! Esc()
@@ -572,6 +614,6 @@ if vim.opt.diff:get() then
 end
 
 vim.o.guicursor = "" -- uncomment for beam cursor
--- vim.cmd("set guicursor+=a:-blinkwait75-blinkoff75-blinkon75")
+--- vim.cmd("set guicursor+=a:-blinkwait75-blinkoff75-blinkon75")
 vim.o.mouse = "n"
--- # vim:ts=4:sw=4:ai:foldmethod=marker:foldlevel=0:
+--- # vim:ts=4:sw=4:ai:foldmethod=marker:foldlevel=0:
