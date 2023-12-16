3ccbdf186f0ce004f87692e5c7d4e9b689b127cd
jlima <lima@localhost>
2023-12-16 15:33:32 -0600

    updates -> setup/setup_linux_deps.sh

--- setup/setup_linux_deps.sh
+++ setup/setup_linux_deps.sh
@@ -12,20 +12,13 @@ echo 'setting up linux dependencies (apt/python)'
 if [[ $EUID -eq 0 ]]; then
   echo 'need root password to install linux dependencies..'
   sudo apt update
+  sudo apt -y upgrade
   if [ $? -ne 0 ]; then
     echo "$0"
     echo 'setting up linux deps as root did not work out...'
     exit 1
   fi
-  sudo apt install -y libssl-dev
-  sudo apt install -y curl
-  sudo apt install -y tree
-  sudo apt install -y build-essential
-  sudo apt install -y zsh
-  sudo apt install -y python3
-  sudo apt install -y python3-pip
-  sudo apt install -y silversearcher-ag
-  sudo python3 -m pip install --upgrade pip
+  sudo apt install -y build-essential libssl-dev curl tree zsh python3 silversearcher-ag python3-pip
 fi
 
 if [[ $EUID -ne 0 ]]; then
@@ -37,15 +30,7 @@ if [[ $EUID -ne 0 ]]; then
     echo 'setting up linux deps as root did not work out...'
     exit 1
   fi
-  sudo apt install -y libssl-dev
-  sudo apt install -y curl
-  sudo apt install -y tree
-  sudo apt install -y build-essential
-  sudo apt install -y python3
-  sudo apt install -y python3-pip
-  sudo apt install -y silversearcher-ag
-  sudo python3 -m pip install --upgrade pip
+  sudo apt install -y build-essential libssl-dev curl tree zsh python3 silversearcher-ag python3-pip
 fi
 
 echo 'done installing linux depenencies'
-
