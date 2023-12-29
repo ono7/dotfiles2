d6fc6fe9d940afb8eb5bcfc1a03de35eb61dc794
jlima <lima@localhost>
2023-12-28 19:11:05 -0600

    updates -> alacritty.toml setup/pip3_freeze.txt setup/setup_linux_deps.sh setup/setup_neovim.sh ...

--- setup/setup_linux_deps.sh
+++ setup/setup_linux_deps.sh
@@ -12,31 +12,13 @@ sudo locale-gen "en_US.UTF-8"
 
 echo 'setting up linux dependencies (apt/python)'
 
-if [[ $EUID -eq 0 ]]; then
-  echo 'need root password to install linux dependencies..'
-  sudo apt update
-  sudo apt -y upgrade
-  if [ $? -ne 0 ]; then
-    echo "$0"
-    echo 'setting up linux deps as root did not work out...'
-    exit 1
-  fi
-  sudo apt install -y build-essential libssl-dev curl tree zsh python3 silversearcher-ag python3-pip fd-find unzip
-fi
-
-if [[ $EUID -ne 0 ]]; then
-  # regular joe attempt
-  echo 'need root password to install linux dependencies..'
-  sudo apt update
-  if [ $? -ne 0 ]; then
-    echo "$0"
-    echo 'setting up linux deps as root did not work out...'
-    exit 1
-  fi
-  sudo apt install -y build-essential libssl-dev curl tree zsh python3 silversearcher-ag python3-pip fd-find unzip
-fi
+sudo apt update
+sudo apt -y upgrade
+sudo apt install -y build-essential libssl-dev curl tree zsh python3 silversearcher-ag python3-pip fd-find unzip
 
 echo 'done installing linux depenencies'
+
+
 
 echo 'setting up nvim'
 
@@ -54,3 +36,18 @@ echo "Neovim setup for linux complete"
 echo "install nvim...."
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+echo "Activating NVM"
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+echo "Installing latest node version"
+
+if type nvm > /dev/null 2>&1; then
+    echo "NVM found. Installing Node.js..."
+    nvm install node
+    echo "installing npm packages......."
+    npm install lua-fmt prettier jsonlint -g
+else
+    echo "NVM not found. Skipping Node.js installation."
+fi
