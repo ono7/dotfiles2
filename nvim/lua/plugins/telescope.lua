ef6782c26d6f3bfdae8735c02d3bd68f2af4d9ca
jlima <lima@localhost>
2024-01-02 23:16:51 -0600

    updates -> nvim/lua/plugins/telescope.lua

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -63,10 +63,15 @@ configs.setup({
 
 -- k({ "n", "x" }, "<c-f>", "<cmd>lua require('telescope.builtin').find_files({ cwd = '.' })<cr>", opt)
 k("n", "<c-p>", require("telescope.builtin").oldfiles, { desc = "[?] Find recently opened files" })
-k({ "n", "x" }, "<leader>ff", "<cmd>lua require('telescope.builtin').find_files({ no_ignore=true, hidden=true })<cr>", opt)
-k({ "n", "x" }, "<c-f>", "<cmd>lua require('telescope.builtin').git_files({ show_untracked = true, no_ignore=false, hidden=true })<cr>",
+k({ "n", "x" }, "<leader>ff", "<cmd>lua require('telescope.builtin').find_files({ no_ignore=true, hidden=true })<cr>",
+  opt)
+k({ "n", "x" }, "<c-f>", "<cmd>lua require('telescope.builtin').find_files( { cwd = vim.fn.expand('%:p:h') })<cr>")
+k({ "n", "x" }, "<leader>fg",
+  "<cmd>lua require('telescope.builtin').git_files({ show_untracked = true, no_ignore=false, hidden=true })<cr>",
   silent)
-k("n", "<leader>g", "<cmd>lua require('telescope.builtin').live_grep{ vimgrep_arguments = { 'rg', '--color=never', '--no-heading', '--with-filename', '--line-number', '--column', '--smart-case', '-u' } }<cr>", opt)
+k("n", "<leader>g",
+  "<cmd>lua require('telescope.builtin').live_grep{ vimgrep_arguments = { 'rg', '--color=never', '--no-heading', '--with-filename', '--line-number', '--column', '--smart-case', '-u' }, use_regex=true }<cr>",
+  opt)
 k("n", "<leader>fd", "<cmd>lua require('telescope.builtin').diagnostics()<cr>", opt)
 
 -- k("n", "<leader>fb", "<cmd>lua require('telescope.builtin').live_grep({grep_open_files = true})<cr>", opt)
