760d8a8518143d785df6b0b2f58b902f1c9f9147
jlima <lima@localhost>
2024-01-07 21:46:46 -0600

    updates -> nvim/lua/my/disabled.lua nvim/lua/my/lazy.lua

--- nvim/lua/my/disabled.lua
+++ nvim/lua/my/disabled.lua
@@ -16,9 +16,9 @@ vim.g.loaded_matchit = 1
 vim.g.loaded_logiPat = 1
 vim.g.loaded_rrhelper = 1
 
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-vim.g.loaded_netrwSettings = 1
+-- vim.g.loaded_netrw = 1
+-- vim.g.loaded_netrwPlugin = 1
+-- vim.g.loaded_netrwSettings = 1
 
 --- provider settings ---
 vim.g.python_host_skip_check = 1
--- nvim/lua/my/lazy.lua
+++ nvim/lua/my/lazy.lua
@@ -36,6 +36,13 @@ require("lazy").setup({
   --     "rcarriga/nvim-notify",
   --   }
   -- },
+  {
+    "stevearc/oil.nvim",
+    config = function()
+      require("oil").setup()
+      vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+    end
+  },
   { "catppuccin/nvim",        name = "catppuccin" },
   {
     "folke/trouble.nvim",
