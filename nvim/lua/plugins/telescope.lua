6a77092ae141de8aa15000e2abbc23ca56ed434d
jlima <lima@localhost>
2024-02-02 15:58:46 -0600

    updates -> nvim/lua/plugins/telescope.lua

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -16,7 +16,6 @@ if not actions_setup then
   return
 end
 
-
 configs.load_extension("git_worktree")
 configs.load_extension("fzf")
 
@@ -28,20 +27,6 @@ local function get_git_root()
   else
     return "."
   end
-end
-
-local function get_visual_selection()
-  local s_start = vim.fn.getpos("'<")
-  local s_end = vim.fn.getpos("'>")
-  local n_lines = math.abs(s_end[2] - s_start[2]) + 1
-  local lines = vim.api.nvim_buf_get_lines(0, s_start[2] - 1, s_end[2], false)
-  lines[1] = string.sub(lines[1], s_start[3], -1)
-  if n_lines == 1 then
-    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3] - s_start[3] + 1)
-  else
-    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3])
-  end
-  return table.concat(lines, ' ')
 end
 
 local find_all_files_cmd = {
@@ -156,15 +141,5 @@ k("n", "<leader>fW", function()
   builtin.grep_string { search = word }
 end, opt)
 
-k("v", "<leader>f", function()
-  builtin.grep_string { search = get_visual_selection() }
-end, opt)
-
--- k(
---   "n",
---   "<leader>fw",
---   function()
---     builtin.live_grep({ cwd = '~/notes', hidden = true, show_untracked = true, no_ignore = true })
---   end,
---   opt
--- )
+-- get what is visual selected and pass it to live_grep, hacky but works
+k('v', '<leader>f', 'y<ESC>:Telescope live_grep default_text=<c-r>0<CR>', silent)
