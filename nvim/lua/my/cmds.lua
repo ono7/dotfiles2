b8e1c9890cf524f3fb17c74abbdd4a26d3736b9b
jlima <lima@localhost>
2024-02-10 14:13:55 -0600

    Refactor cmds for git commit messages on enter, do not go into insertmode

--- nvim/lua/my/cmds.lua
+++ nvim/lua/my/cmds.lua
@@ -54,9 +54,9 @@ c({ "BufEnter" }, {
   callback = function()
     vim.wo.spell = true
     vim.api.nvim_win_set_cursor(0, { 1, 0 })
-    if vim.fn.getline(1) == '' then
-      vim.cmd 'startinsert!'
-    end
+    -- if vim.fn.getline(1) == '' then
+    --   vim.cmd 'startinsert!'
+    -- end
   end,
 })
 
