50bd53a218be1e041109d2bb12b312cf85852389
jlima <lima@localhost>
2024-06-05 02:18:02 -0500

    fix live-server, issue not loading

--- nvim/lua/plugins/live_server.lua
+++ nvim/lua/plugins/live_server.lua
@@ -1,7 +1,6 @@
-local ok, config = pcall(require, 'live-server')
-
-if not ok then
-  print("live server not loaded")
+local live_server_ok, config = pcall(require, "live-server")
+if not live_server_ok then
+  print("Error in pcall live-server -> ~/.dotfiles/nvim/lua/plugins/live_server.lua")
   return
 end
 
