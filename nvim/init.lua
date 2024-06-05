c8511a52be61d5e8fb8d1ae3ffb4a702e92af3a2
jlima <lima@localhost>
2024-06-05 00:17:02 -0500

    cleanup

--- nvim/init.lua
+++ nvim/init.lua
@@ -98,20 +98,6 @@ local function get_git_root()
   return vim.fn.fnamemodify(dot_git_path, ":h")
 end
 
-local function get_visual_selection()
-  local s_start = vim.fn.getpos("'<")
-  local s_end = vim.fn.getpos("'>")
-  local n_lines = math.abs(s_end[2] - s_start[2]) + 1
-  local lines = vim.api.nvim_buf_get_lines(0, s_start[2] - 1, s_end[2], false)
-  lines[1] = string.sub(lines[1], s_start[3], -1)
-  if n_lines == 1 then
-    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3] - s_start[3] + 1)
-  else
-    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3])
-  end
-  return table.concat(lines, '\n')
-end
-
 vim.api.nvim_create_user_command("CdGitRoot", function()
   vim.api.nvim_set_current_dir(get_git_root())
 end, {})
@@ -190,11 +176,8 @@ end, silent)
 
 local function check_buf(bufnr)
   --- checks if this is a valid buffer that we can save to ---
-  local buftype = vim.api.nvim_buf_get_option(bufnr, 'buftype')
-  local bufhidden = vim.api.nvim_buf_get_option(bufnr, 'bufhidden')
   local bufname = vim.api.nvim_buf_get_name(bufnr)
-
-  if buftype == 'nofile' or bufhidden == 'hide' or bufname == '' then
+  if bufname == '' then
     return false
   end
   return true
