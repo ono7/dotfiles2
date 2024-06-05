0b9eb5d2a83bf31b7c0ead57bb1adc73ff53e214
jlima <jlima@mbp>
2024-06-05 13:21:56 -0500

    removed run to cursor keybinding in dap.lua

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -52,7 +52,7 @@ require("nvim-dap-virtual-text").setup {
 -- end
 
 vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
-vim.keymap.set("n", "<space>gb", dap.run_to_cursor)
+-- vim.keymap.set("n", "<space>gb", dap.run_to_cursor)
 
 -- Eval var under cursor
 vim.keymap.set("n", "<space>?", function()
