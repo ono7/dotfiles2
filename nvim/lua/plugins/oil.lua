8aad3416f951f5926d2845be8e890341ff870ab1
jlima <lima@localhost>
2024-06-07 12:09:37 -0500

    edits to oil.lua

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -8,13 +8,19 @@ end
 
 oil_config.setup {
   columns = { "icon" },
-  keymaps = {
-    ["<C-h>"] = false,
-    ["<M-h>"] = "actions.select_split",
-  },
+  -- keymaps = {
+  --   ["<C-h>"] = false,
+  --   ["<M-h>"] = "actions.select_split",
+  -- },
   view_options = {
     show_hidden = true,
   },
+  -- Skip the confirmation popup for simple operations (:help oil.skip_confirm_for_simple_edits)
+  skip_confirm_for_simple_edits = true,
+
+  -- Selecting a new/moved/renamed file or directory will prompt you to save changes first
+  -- (:help prompt_save_on_select_new_entry)
+  prompt_save_on_select_new_entry = false
 }
 
 -- Open parent directory in current window
