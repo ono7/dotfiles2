0b358357e1d11a8feca42082332abc7ce6d6fa40
lima <lima@mbp>
2024-06-27 00:50:00 -0500

    .

--- setup.sh
+++ setup.sh
@@ -16,3 +16,6 @@ ln -sf "$PWD/config.nix" "$XDG_CONFIG_HOME"/nixpkgs/config.nix
 
 # install Nix packages from config.nix
 nix-env -iA nixpkgs.myPackages
+
+sudo locale-gen en_US.UTF-8
+sudo update-locale LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8
