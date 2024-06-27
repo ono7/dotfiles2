64db9ddcb767b2fec3ab24c00f6c3ee309ce5067
lima <lima@mbp>
2024-06-26 22:39:09 -0500

    added config.nix for nix containers

---
+++ config.nix
@@ -0,0 +1,18 @@
+{
+  packageOverrides = pkgs: with pkgs; {
+    myPackages = pkgs.buildEnv {
+      name = "myPackages";
+      paths = [
+        bash-completion
+        neovim
+        go
+        nodejs_22
+        starship
+        fd
+        ripgrep
+        fzf
+        lazygit
+      ];
+    };
+  };
+}
--- devcontainer.sh
+++ devcontainer.sh
@@ -11,4 +11,4 @@ pwd
 cd $d
 
 # install Nix packages from config.nix
-# nix-env -iA nixpkgs.myPackages
+nix-env -iA nixpkgs.myPackages
