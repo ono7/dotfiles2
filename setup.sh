e56058e2893d0f32ac58234a786457b1c970b5c8
lima <lima@mbp>
2024-06-27 00:09:45 -0500

    updates

--- setup.sh
+++ setup.sh
@@ -8,10 +8,9 @@ mkdir -p "$XDG_CONFIG_HOME"/nixpkgs
 
 ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME"/nvim
 
-ln -sf "$PWD/.bash_profile" "$HOME"/.bash_profile
-ln -sf "$PWD/.bashrc" "$HOME"/.bashrc
-ln -sf "$PWD/.inputrc" "$HOME"/.inputrc
-ln -sf "$PWD/.tmux.conf" "$HOME"/.tmux.conf
+ln -sf "$PWD/bashrc" "$HOME"/.bashrc
+ln -sf "$PWD/inputrc" "$HOME"/.inputrc
+ln -sf "$PWD/tmux/tmux.conf" "$HOME"/.tmux.conf
 ln -sf "$PWD/config.nix" "$XDG_CONFIG_HOME"/nixpkgs/config.nix
 
 # install Nix packages from config.nix
