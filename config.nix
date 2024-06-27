fe218eb9a235320b6732bf493c7dddcd1e215f4d
lima <lima@mbp>
2024-06-27 17:15:21 -0500

    updates

--- config.nix
+++ config.nix
@@ -6,18 +6,16 @@
       paths = [
         bash-completion
         neovim
-        go
-        nodejs_22
+        # go
+        # nodejs_22
         starship
         fd
         ripgrep
         fzf
-	python3
-	ruff
-	# for building docker images
-	docker-client
-	gnumake
-	git
+        python3
+        ruff
+        # for building docker images
+        gnumake
       ];
     };
   };
